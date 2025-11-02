ed284760944cd9e94b664295fb8121c3c55afd89
tlasu <sukazuki.mail@gmail.com>
2025-11-02 21:24:20 +0900

    🔧 chore: Update console log for base path and simplify image source path in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 import Image from "next/image";
 
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-console.log(basePath); // /deploy_github
-
 export default function Home() {
+  console.log(`basePath: ${basePath}`); // /deploy_github/vercel.svg
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src={`${basePath}/next.svg`}
+          src="/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
