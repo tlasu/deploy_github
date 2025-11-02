7ff37f72714ae737a89fc5e81c9900a6d6790173
tlasu <sukazuki.mail@gmail.com>
2025-11-02 21:26:34 +0900

    🔧 chore: Update image source path in Home component to use dynamic base path

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src={`${basePath}/next.svg`}
           alt="Next.js logo"
           width={100}
           height={20}
